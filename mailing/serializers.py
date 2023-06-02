b9ad3c3ee5ba3e5ce7194937ef08f3bc31492a18
timonen87 <timmonen.as@gmail.com>
2023-06-02 23:26:56 +0300

    start app smsApp

---
+++ mailing/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Client, Mailing, Message
+
+class MailingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mailing
+        fields = '__all__'
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
